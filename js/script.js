633131b98237205b00f02a97130cb38b9004c5e7
paula armengol <p.armengol6@gmail.com>
2022-10-17 02:02:59 -0300

    modificacion menor en form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Validacion de formulario
 const btnEnviar = document.getElementById('btn-enviar');
 
 const validación = (e) => {
@@ -5,24 +6,24 @@ const validación = (e) => {
   const nombreDeUsuario = document.getElementById('usuario');
   const direcciónEmail = document.getElementById('email');
   if (usuario.value === "") {
-    alert("Por favor, escribe tu nombre de usuario.");
+    alert("Por favor, escribi tu nombre.");
     usuario.focus();
     return false;
   }
     
   if (email.value === "") {
-    alert("Por favor, escribe tu correo electrónico");
+    alert("Por favor, escribi tu email");
     email.focus();
     return false;
   }
 
   if (!emailVálido(email.value)) {
-    alert("Por favor, escribe un correo electrónico válido");
+    alert("Por favor, chequea que tu email sea válido");
     emailAddress.focus();
     return false;
   }
   
-  return true; //Se pueden enviar los datos del formulario al servidor
+  return true; 
 }
 
 const emailVálido = email => {
