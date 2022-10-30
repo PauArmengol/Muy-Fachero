023e6b73c83d0dba2ce82525593ebfe4717a93b4
paula armengol <p.armengol6@gmail.com>
2022-10-30 17:01:36 -0300

    cambios validacion js

--- js/validacion.js
+++ js/validacion.js
@@ -1,94 +1,31 @@
 //Validacion de formulario
 
 
-const formulario = document.getElementById('formulario');
-const inputs = document.querySelectorAll('#formulario input');
+const btnEnviar = document.getElementById('btn-enviar');
 
-const expresiones = {
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, 
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,16}$/ // 7 a 16 numeros.
-}
+function validarFormulario() {
+    btnEnviar.addEventListener("click", (e) => {
+        e.preventDefault();
+        const nombreUsuario = document.getElementById(`nombre`);
+        const emailUsuario = document.getElementById('email');
+        const emailValido = email => {
+          return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+        }
+        const mensajeUsuario = document.getElementById('mensaje');
+        if(nombreUsuario.value === "") {
+          alert("Ingresa tu nombre");
+        } else if(emailUsuario.value === "") {
+          alert("Ingresa tu email"); 
+        } else if(!emailValido(emailUsuario.value)) {
+          alert("Ingresa un email valido");
+        } else if(mensajeUsuario.value === "") {
+          alert("Ingresa tu mensaje");
+        } else {
+          alert("Mensaje enviado");
+        }
+    });
+};
 
-
-
-
-
-const validarFormulario = (e) => {
-  switch (e.target.name) {
-    case "nombre":
-      if(expresiones.nombre.test(e.target.value)) {
-
-
-      } else {
-        document.getElementById(`grupo__nombre`).classList.add(`formulario__grupo-incorrecto`);
-      }
-
-    break;
-
-    case "telefono": 
-
-    break;
-
-    case "email": 
-
-    break;
-
-    case "mensaje": 
-
-    break;
-  }
-}
-
-inputs.forEach((input) => {
-  input.addEventListener('keyup', validarFormulario);
-  input.addEventListener('blur', validarFormulario);
-});
+btnEnviar.addEventListener('click', validarFormulario());
 
 
-formulario.addEventListener(`submit`, (e) => {
-  e.preventDefault(); 
-});
-
-
-
-
-
-
-
-
-
-
-
-/*const btnEnviar = document.getElementById('btn-enviar');
-
-const validación = (e) => {
-  e.preventDefault();
-  const nombreDeUsuario = document.getElementById('usuario');
-  const direcciónEmail = document.getElementById('email');
-  if (usuario.value === "") {
-    alert("Por favor, escribi tu nombre.");
-    usuario.focus();
-    return false;
-  }
-    
-  if (email.value === "") {
-    alert("Por favor, escribi tu email");
-    email.focus();
-    return false;
-  }
-
-  if (!emailVálido(email.value)) {
-    alert("Por favor, chequea que tu email sea válido");
-    emailAddress.focus();
-    return false;
-  }
-  
-  return true; 
-}
-
-const emailVálido = email => {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-}
-
-submitBtn.addEventListener('click', validate);   */
